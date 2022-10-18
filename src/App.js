cfbd002c450051b629dddb144cd3aa64b29a4ec1
maithilibrid <yashbrid03@gmail.com>
2022-10-18 23:36:46 +0530

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEdit, faTrash, faUser, faList, faSignOutAlt, faPlus, faTachometerAlt, faBook, faCode, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faEdit,
+  faTrash,
+  faUser,
+  faList,
+  faSignOutAlt,
+  faPlus,
+  faTachometerAlt,
+  faBook,
+  faCode,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
-import Dashboard from './components/dashboard';
+import Dashboard from "./components/dashboard";
 import BooksListContainer from "./components/book-list-container";
 import BookForm from "./components/book-form";
-import About from './components/about';
-import NotFound from './components/not-found';
+import NotFound from "./components/not-found";
 
 library.add(faEye);
 library.add(faEdit);
@@ -28,33 +39,36 @@ library.add(faCode);
 library.add(faQuestionCircle);
 
 const App = () => {
-
   return (
     <Router>
       <ToastContainer />
       <div className="cont">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <img className="app-logo" src={logo} width="60" height="60" alt="ReactJS" />
+          <img
+            className="app-logo"
+            src={logo}
+            width="60"
+            height="60"
+            alt="ReactJS"
+          />
           <div className="collpase nav-collapse">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
-                <Link to="/" className="nav-link"><FontAwesomeIcon icon='tachometer-alt' /> Dashboard</Link>
+                <Link to="/" className="nav-link">
+                  <FontAwesomeIcon icon="tachometer-alt" /> Dashboard
+                </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/books" className="nav-link"><FontAwesomeIcon icon='book' /> Books</Link>
+                <Link to="/books" className="nav-link">
+                  <FontAwesomeIcon icon="book" /> Books
+                </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/book" className="nav-link"><FontAwesomeIcon icon='plus' /> New Book</Link>
+                <Link to="/book" className="nav-link">
+                  <FontAwesomeIcon icon="plus" /> New Book
+                </Link>
               </li>
             </ul>
-            {<ul className="navbar-nav mr-auto pull-right">
-              <li className="navbar-item">
-                <Link to="/about" className="nav-link"><FontAwesomeIcon icon='question-circle' /></Link>
-              </li>
-              <li className="navbar-item">
-                <a href="https://github.com/BnSalahFahmi/book-store" className="nav-link"><FontAwesomeIcon icon='code' /></a>
-              </li>
-            </ul>}
           </div>
         </nav>
         <div className="content">
@@ -63,13 +77,12 @@ const App = () => {
             <Route path="/books" component={BooksListContainer} />
             <Route path="/book" component={BookForm} />
             <Route path="/book/:id" component={BookForm} />
-            <Route path="/about" component={About} />
             <Route component={NotFound} />
           </Switch>
         </div>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/not-found.js
+++ src/components/not-found.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import photo from '../assets/img/photo.jpg';
-import pageNotFound from '../assets/img/page-not-found.png';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+// import photo from '../assets/img/photo.jpg';
+import pageNotFound from "../assets/img/page-not-found.png";
 
 export class NotFound extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="page-header">
-                    <img className="not-found" src={pageNotFound} />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="page-header">
+          <img className="not-found" src={pageNotFound} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotFound;
